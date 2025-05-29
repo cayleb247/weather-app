a5f0f4e7a2ed5f474f011d158fbe746abb4862a8
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-05-29 00:34:10 -0400

    Add fog svg

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ let weatherIcons = {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M440-760v-160h80v160h-80Zm266 110-56-56 113-114 56 57-113 113Zm54 210v-80h160v80H760Zm3 299L650-254l56-56 114 112-57 57ZM254-650 141-763l57-57 112 114-56 56Zm-14 450h180q25 0 42.5-17.5T480-260q0-25-17-42.5T421-320h-51l-20-48q-14-33-44-52.5T240-440q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T40-320q0-83 58.5-141.5T240-520q60 0 109.5 32.5T423-400q58 0 97.5 43T560-254q-2 57-42.5 95.5T420-120H240Zm320-134q-5-20-10-39t-10-39q45-19 72.5-59t27.5-89q0-66-47-113t-113-47q-60 0-105 39t-53 99q-20-5-41-9t-41-9q14-88 82.5-144T480-720q100 0 170 70t70 170q0 77-44 138.5T560-254Zm-79-226Z"/></svg>',
   partlycloudynight:
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M504-425Zm20 385H420l20-12.5q20-12.5 43.5-28t43.5-28l20-12.5q81-6 149.5-49T805-285q-86-8-163-43.5T504-425q-61-61-97-138t-43-163q-77 43-120.5 118.5T200-444v12l-12 5.5q-12 5.5-26.5 11.5T135-403.5l-12 5.5q-2-11-2.5-23t-.5-23q0-146 93-257.5T450-840q-18 99 11 193.5T561-481q71 71 165.5 100T920-370q-26 144-138 237T524-40Zm-284-80h180q25 0 42.5-17.5T480-180q0-25-17-42.5T422-240h-52l-20-48q-14-33-44-52.5T240-360q-50 0-85 34.5T120-240q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T40-240q0-83 58.5-141.5T240-440q60 0 109.5 32.5T423-320q57 2 97 42.5t40 97.5q0 58-41 99t-99 41H240Z"/></svg>',
+  fog: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3"><path d="M720-200q-17 0-28.5-11.5T680-240q0-17 11.5-28.5T720-280q17 0 28.5 11.5T760-240q0 17-11.5 28.5T720-200ZM280-80q-17 0-28.5-11.5T240-120q0-17 11.5-28.5T280-160q17 0 28.5 11.5T320-120q0 17-11.5 28.5T280-80Zm-40-120q-17 0-28.5-11.5T200-240q0-17 11.5-28.5T240-280h360q17 0 28.5 11.5T640-240q0 17-11.5 28.5T600-200H240ZM400-80q-17 0-28.5-11.5T360-120q0-17 11.5-28.5T400-160h280q17 0 28.5 11.5T720-120q0 17-11.5 28.5T680-80H400ZM300-320q-91 0-155.5-64.5T80-540q0-83 55-145t136-73q32-57 87.5-89.5T480-880q90 0 156.5 57.5T717-679q69 6 116 57t47 122q0 75-52.5 127.5T700-320H300Zm0-80h400q42 0 71-29t29-71q0-42-29-71t-71-29h-60v-40q0-66-47-113t-113-47q-48 0-87.5 26T333-704l-10 24h-25q-57 2-97.5 42.5T160-540q0 58 41 99t99 41Zm180-200Z"/></svg>',
 };
 
 // Location Text
@@ -46,12 +47,18 @@ function populateDayData(response) {
   const lowText = document.querySelector(".low");
   const highText = document.querySelector(".high");
   const conditionsText = document.querySelector(".conditions");
+  const humidityStatText = document.querySelector(".humid-stat");
+  const uvIndexStatText = document.querySelector(".uv-stat");
+  const precipitationStatText = document.querySelector(".precipitation h3");
 
   let today = response.days[0];
   tempText.textContent = `${today.temp}°`;
   lowText.textContent = `L:${today.tempmin}°`;
   highText.textContent = `H:${today.tempmax}°`;
   conditionsText.textContent = today.conditions;
+  humidityStatText.textContent = today.humidity + "%";
+  uvIndexStatText.textContent = today.uvindex;
+  precipitationStatText.textContent = today.precip + '"';
   console.log(today);
 }
 
@@ -66,20 +73,24 @@ function populateHourData(response) {
 }
 
 function populateWeekData(response) {
-    const dateText = document.querySelectorAll(".date-text");
-    const dayIcons = document.querySelectorAll(".weather-icon-day");
-    const dayTempText = document.querySelectorAll(".day-temp-text");
-    const weekData = response.days;
-    const lowTempText = document.querySelectorAll(".forecast-day .low");
-    const highTempText = document.querySelectorAll(".forecast-day .high");
+  const dateText = document.querySelectorAll(".date-text");
+  const dayIcons = document.querySelectorAll(".weather-icon-day");
+  const dayTempText = document.querySelectorAll(".day-temp-text");
+  const weekData = response.days;
+  const lowTempText = document.querySelectorAll(".forecast-day .low");
+  const highTempText = document.querySelectorAll(".forecast-day .high");
 
-    for (let i=0; i<7; i++) {
-        dateText[i].textContent = weekData[i+1].datetime.split("-").slice(1,3).join("/");
-        dayIcons[i].innerHTML = weatherIcons[weekData[i+1].icon.split("-").join("")];
-        dayTempText[i].textContent = weekData[i+1].temp + "°";
-        lowTempText[i].textContent = `L:${weekData[i+1].tempmin}°`;
-        highTempText[i].textContent = `H:${weekData[i+1].tempmax}°`;
-    }
+  for (let i = 0; i < 7; i++) {
+    dateText[i].textContent = weekData[i + 1].datetime
+      .split("-")
+      .slice(1, 3)
+      .join("/");
+    dayIcons[i].innerHTML =
+      weatherIcons[weekData[i + 1].icon.split("-").join("")];
+    dayTempText[i].textContent = weekData[i + 1].temp + "°";
+    lowTempText[i].textContent = `L:${weekData[i + 1].tempmin}°`;
+    highTempText[i].textContent = `H:${weekData[i + 1].tempmax}°`;
+  }
 }
 
 function displayError() {}
@@ -92,26 +103,26 @@ function populateData(response) {
 }
 
 function fetchData(search) {
-    fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?key=${API_KEY}`,
-        { mode: "cors" }
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error();
-          } else {
-            return response.json();
-          }
-        })
-        .then((response) => {
-          console.log(response);
-          console.log(response.resolvedAddress);
-          populateData(response);
-        })
-        .catch((error) => {
-          console.log("location not found.");
-          // displayError();
-        });
+  fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?key=${API_KEY}`,
+    { mode: "cors" }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error();
+      } else {
+        return response.json();
+      }
+    })
+    .then((response) => {
+      console.log(response);
+      console.log(response.resolvedAddress);
+      populateData(response);
+    })
+    .catch((error) => {
+      console.log("location not found.");
+      // displayError();
+    });
 }
 
 locationForm.addEventListener("submit", (event) => {
@@ -123,4 +134,4 @@ tempUnitButton.addEventListener("click", () => {});
 
 // On Start
 
-fetchData("Roanoke");
+fetchData("New York");
